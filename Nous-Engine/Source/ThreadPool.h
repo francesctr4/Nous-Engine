268ad276dad50b6818e8bc9bb4c8268b293fe11a
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2024-09-29 23:10:37 +0200

    Update ThreadPool.h

--- Nous-Engine/Source/ThreadPool.h
+++ Nous-Engine/Source/ThreadPool.h
@@ -6,23 +6,60 @@
 
 #include "concurrentqueue.h"
 
+// Adapt the thread pool size according to the hardware capabilities.
 #define MAX_THREADS std::thread::hardware_concurrency()
 
+// Lock-free thread-safe concurrent FIFO queue. Multiple producer, multiple consumer. Third Party.
 template<typename T>
 using TSQueue = moodycamel::ConcurrentQueue<T>;
 
-// Lock-free FIFO queue
-
 class ThreadPool
 {
-	std::atomic_bool done;
+public:
+
+	// Constructor.
+	ThreadPool() : done(false)
+	{
+		try
+		{
+			for (unsigned int i = 0; i < MAX_THREADS; ++i)
+			{
+				threads.push_back(std::jthread(&ThreadPool::WorkerThread, this));
+			}
+		}
+		catch (...)
+		{
+			done = true;
+			throw;
+		}
+	}
+
+	// Destructor.
+	~ThreadPool()
+	{
+		done = true;
+	}
+
+	// Submit a task to be handled by threads on the thread pool.
+	template<typename T>
+	void Submit(T task)
+	{
+		workQueue.enqueue(std::function<void()>(task));
+	}
+
+private:
+
 	TSQueue<std::function<void()>> workQueue;
+
 	std::vector<std::jthread> threads;
-	void worker_thread()
+	std::atomic_bool done;
+
+	void WorkerThread()
 	{
 		while (!done)
 		{
 			std::function<void()> task;
+
 			if (workQueue.try_dequeue(task))
 			{
 				task();
@@ -33,30 +70,5 @@ class ThreadPool
 			}
 		}
 	}
-public:
-	ThreadPool() : done(false)
-	{
-		try
-		{
-			for (unsigned int i = 0; i < MAX_THREADS; ++i)
-			{
-				threads.push_back(
-					std::jthread(&ThreadPool::worker_thread, this));
-			}
-		}
-		catch (...)
-		{
-			done = true;
-			throw;
-		}
-	}
-	~ThreadPool()
-	{
-		done = true;
-	}
-	template<typename FunctionType>
-	void submit(FunctionType f)
-	{
-		workQueue.enqueue(std::function<void()>(f));
-	}
+
 };
