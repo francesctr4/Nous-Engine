83601b7fcb6019911bd792fab01e8e54e0753d40
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2024-11-22 21:43:15 +0100

    Vulkan Shader Modules creation finished

--- Nous-Engine/Source/VulkanShaderUtils.cpp
+++ Nous-Engine/Source/VulkanShaderUtils.cpp
@@ -3,51 +3,59 @@
 
 #include <format>
 #include "MemoryManager.h"
+#include "FileHandle.h"
 
 bool CreateShaderModule(VulkanContext* vkContext, std::string name, std::string typeStr, VkShaderStageFlagBits shaderStageFlag, uint32 stageIndex, VulkanShaderStage* shaderStages)
 {
     bool ret = true;
 
     // Build filename.
-    std::string filename = std::format("Assets/Shaders/{}.{}.spv", name, typeStr);
+    std::string filename = std::format("Library/Shaders/{}.{}.spv", name, typeStr);
 
-    MemoryManager::ZeroMemory(&shaderStages[stageIndex].shaderStageCreateInfo, sizeof(VkShaderModuleCreateInfo));
-    shaderStages[stageIndex].shaderStageCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    MemoryManager::ZeroMemory(&shaderStages[stageIndex].shaderModuleCreateInfo, sizeof(VkShaderModuleCreateInfo));
+    shaderStages[stageIndex].shaderModuleCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
 
     // Obtain file handle.
-    file_handle handle;
-    if (!filesystem_open(file_name, FILE_MODE_READ, true, &handle)) {
-        KERROR("Unable to read shader module: %s.", file_name);
-        return false;
+    FileHandle handle;
+
+    if (!handle.Open(filename, FileMode::READ, true)) 
+    {
+        NOUS_ERROR("Unable to read shader module: %s.", filename.c_str());
+        ret = false;
     }
 
     // Read the entire file as binary.
-    u64 size = 0;
-    u8* file_buffer = 0;
-    if (!filesystem_read_all_bytes(&handle, &file_buffer, &size)) {
-        KERROR("Unable to binary read shader module: %s.", file_name);
-        return false;
+    uint64 size = 0;
+    char* fileBuffer = nullptr;
+
+    if (!handle.ReadAllBytes(&fileBuffer, &size) || size == 0)
+    {
+        NOUS_ERROR("Unable to binary read shader module: %s.", filename.c_str());
+        handle.Close();
+
+        ret = false;
     }
-    shader_stages[stage_index].create_info.codeSize = size;
-    shader_stages[stage_index].create_info.pCode = (u32*)file_buffer;
+
+    shaderStages[stageIndex].shaderModuleCreateInfo.codeSize = size;
+    shaderStages[stageIndex].shaderModuleCreateInfo.pCode = reinterpret_cast<const uint32*>(fileBuffer);
 
     // Close the file.
-    filesystem_close(&handle);
-    VK_CHECK(vkCreateShaderModule(
-        context->device.logical_device,
-        &shader_stages[stage_index].create_info,
-        context->allocator,
-        &shader_stages[stage_index].handle));
+    handle.Close();
+
+    VK_CHECK(vkCreateShaderModule(vkContext->device.logicalDevice, &shaderStages[stageIndex].shaderModuleCreateInfo,
+        vkContext->allocator, &shaderStages[stageIndex].handle));
 
     // Shader stage info
-    kzero_memory(&shader_stages[stage_index].shader_stage_create_info, sizeof(VkPipelineShaderStageCreateInfo));
-    shader_stages[stage_index].shader_stage_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-    shader_stages[stage_index].shader_stage_create_info.stage = shader_stage_flag;
-    shader_stages[stage_index].shader_stage_create_info.module = shader_stages[stage_index].handle;
-    shader_stages[stage_index].shader_stage_create_info.pName = "main";
-    if (file_buffer) {
-        kfree(file_buffer, sizeof(u8) * size, MEMORY_TAG_STRING);
-        file_buffer = 0;
+    MemoryManager::ZeroMemory(&shaderStages[stageIndex].shaderStageCreateInfo, sizeof(VkPipelineShaderStageCreateInfo));
+    shaderStages[stageIndex].shaderStageCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+
+    shaderStages[stageIndex].shaderStageCreateInfo.stage = shaderStageFlag;
+    shaderStages[stageIndex].shaderStageCreateInfo.module = shaderStages[stageIndex].handle;
+    shaderStages[stageIndex].shaderStageCreateInfo.pName = "main";
+
+    if (fileBuffer) 
+    {
+        NOUS_DELETE_ARRAY(fileBuffer, size, MemoryManager::MemoryTag::FILE);
     }
 
     return ret;
