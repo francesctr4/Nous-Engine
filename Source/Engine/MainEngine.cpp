691e9eef556e89a035b42d6b8623223bc65255ef
Francesc Teruel Rodríguez <99948892+francesctr4@users.noreply.github.com>
2026-03-21 23:03:40 +0100

    [CI] Guard __declspec in MainEngine.cpp for Linux/macOS

--- Source/Engine/MainEngine.cpp
+++ Source/Engine/MainEngine.cpp
@@ -1,1 +1,5 @@
+#ifdef _WIN32
 extern "C" __declspec(dllexport) const char* NousEngine_Version = "0.4.0";
+#else
+extern "C" __attribute__((visibility("default"))) const char* NousEngine_Version = "0.4.0";
+#endif
